aa906b37062b6d7937a9c0f8e7b62b32afe4bdeb
Magier Marcin <marcinmagier@gmail.com>
2016-10-28 11:55:34 +0200

    create sysroot directory for common files

--- sysroot_mips/usr/share/gigaset/usr/bin/show_sensors.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -f /usr/gigaset/data/endnodes ]; then
-	cat /usr/gigaset/data/endnodes
-else
-	echo "Endnodes database is empty!"
-fi
--- sysroot_mips/usr/share/gigaset/usr/bin/unpair.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-rm -f /usr/gigaset/data/cert/cert.crt
-rm -f /usr/gigaset/data/cache/*
-sync
-
